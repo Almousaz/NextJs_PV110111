15feac85d8bbe32fe827e5ff1804a94eacd810da
Ali Mousazada <mousazada.ali@gmail.com>
2025-03-17 11:49:48 -0400

    setup and structure

---
+++ Ecommerce/my-app/src/app/layout.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+import '../styles/globals.css';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+                <Header />
+                <main className="main-content">{children}</main>
+                <Footer />
+      </body>
+    </html>
+  );
+}
